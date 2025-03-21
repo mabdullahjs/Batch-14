fa3c52722a802bb52074e394440902e007ab5e81
mabdullahjs <mabdullah2037@outlook.com>
2025-03-21 08:35:00 +0500

    api fetch

--- 04-Asynchronous/app.js
+++ 04-Asynchronous/app.js
@@ -108,22 +108,91 @@
 //     console.log(err);
 //   });
 
-function shaadiScnz(bankBalance) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (bankBalance > 1000000) {
-        resolve("shaadi Mubarak");
-      } else {
-        reject("istekhara ma naa aya haa.cast sa bhar nahi krta hum");
-      }
-    }, 1000);
-  });
+// function shaadiScnz(bankBalance) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (bankBalance > 1000000) {
+//         resolve("shaadi Mubarak");
+//       } else {
+//         reject("istekhara ma naa aya haa.cast sa bhar nahi krta hum");
+//       }
+//     }, 0);
+//   });
+// }
+
+// shaadiScnz(2500000)
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// function dataMangwalo(isLoggedIn) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (isLoggedIn === "yes") {
+//         resolve("data agaya");
+//       } else {
+//         reject("data nahi aya");
+//       }
+//     }, 1000);
+//   });
+// }
+
+// promise chaining
+
+// dataMangwalo("yes")
+//   .then((res) => {
+//     console.log("resolve(then) ===>", res);
+//     return res + " update krdia ha";
+//   })
+//   .then((res) => {
+//     console.log("second (then) ===>", res);
+//   })
+//   .catch((err) => {
+//     console.log("reject(catch) ===>", err);
+//   });
+
+// async await
+
+// async function asyncFunc() {
+//   try {
+//     const response = await dataMangwalo("yes");
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// asyncFunc();
+
+// API
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log("error ==>", err);
+//   });
+
+const div = document.querySelector(".container");
+
+async function getUsers() {
+  try {
+    const data = await fetch("https://jsonplaceholder.typicode.com/users");
+    const response = await data.json();
+    console.log(response);
+    response.map((item) => {
+      div.innerHTML += `<p>${item.name}</p>`;
+    });
+  } catch (error) {
+    console.log("error ===> ", error);
+  }
 }
 
-shaadiScnz(2500000)
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+getUsers();
